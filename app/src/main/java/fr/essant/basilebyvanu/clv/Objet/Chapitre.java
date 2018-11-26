004b6b226ae7b6c2cb37fd5ca5b05bd75ea691be
Basile Bruhat <basile.bruhat@etudiant.univ-rennes1.fr>
2018-11-26 18:44:59 +0100

    Update : add JSON, Chapitre, menuChapitre

--- app/src/main/java/fr/essant/basilebyvanu/clv/Objet/Chapitre.java
+++ app/src/main/java/fr/essant/basilebyvanu/clv/Objet/Chapitre.java
@@ -1,4 +1,40 @@
 package fr.essant.basilebyvanu.clv.Objet;
 
 public class Chapitre {
+
+    private int temps;
+    private String nom;
+    private int numero;
+
+
+
+    public Chapitre(String nom, int temps, int numero) {
+        this.temps = temps;
+        this.nom = nom;
+        this.numero = numero;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getTemps() {
+        return temps;
+    }
+
+    public void setTemps(int temps) {
+        this.temps = temps;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
 }
--- app/src/main/java/fr/essant/basilebyvanu/clv/Objet/MenuChapitre.java
+++ app/src/main/java/fr/essant/basilebyvanu/clv/Objet/MenuChapitre.java
@@ -1,4 +1,59 @@
 package fr.essant.basilebyvanu.clv.Objet;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+
 public class MenuChapitre {
+
+    private ArrayList<Chapitre> listChapitres;
+    private JSONObject jsonObject;
+    private JSONArray jsonArray;
+
+    public MenuChapitre(){
+
+    }
+
+    public static ArrayList<Chapitre> readFromJsonFile(String filename){
+        ArrayList<Chapitre> result = new ArrayList<>();
+        try {
+            String text = new String(Files.readAllBytes(Paths.get(filename)), StandardCharsets.UTF_8);
+
+            JSONObject object = new JSONObject(text);
+            JSONArray arr = object.getJSONArray("chapitre");
+
+            for (int i = 0; i < arr.length(); i++ ) {
+
+                String nom = arr.getJSONObject(i).getString("nom");
+                int temps = arr.getJSONObject(i).getInt("temps");
+                int numero = arr.getJSONObject(i).getInt("numero");
+
+                Chapitre chapitre = new Chapitre(nom, temps, numero);
+                result.add(chapitre);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+    public ArrayList<Chapitre> getListChapitres() {
+        return listChapitres;
+    }
+
+    public void addChapitres(Chapitre chapitres) {
+        this.listChapitres.add(chapitres);
+    }
 }
