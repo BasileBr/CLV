343842f0ac4435072c5f6cf2afc810f5e099790b
Basile Bruhat <basile.bruhat@etudiant.univ-rennes1.fr>
2018-12-05 08:33:51 +0100

    Update : change link Fix : rm commentaires

--- app/src/main/java/fr/essant/basilebyvanu/clv/Model/NameViewModel.java
+++ app/src/main/java/fr/essant/basilebyvanu/clv/Model/NameViewModel.java
@@ -1,59 +1,14 @@
 package fr.essant.basilebyvanu.clv.Model;
 
-import android.app.Application;
-import android.arch.lifecycle.AndroidViewModel;
-import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class NameViewModel extends ViewModel {
 
-    //public MutableLiveData<Integer> mpos;
 
-//
-//    //creation d'un LiveData Object pour stocker la position de la vidéo
-//    private LiveData<Integer> mCurrentPosition;
-//
-//    //récupérer la position de la vidéo
-//    public LiveData<Integer> getmCurrentPosition() {
-//        if (mCurrentPosition == null) {
-//            mCurrentPosition = new MutableLiveData<Integer>();
-//        }
-//        return mCurrentPosition;
-//    }
-//
-//    // modifier la position de la vidéo
-//    public void setPosition(int pos){
-//        mCurrentPosition.setValue(new Integer(pos));
-//    }
-//
-//// Rest of the ViewModel...
-//}
-//    public NameViewModel(Application app)
-//    {   super(app);
-//        mpos = new MutableLiveData<>();
-//        this.setPosition(0);
-//
-//    }
-//
-//    public LiveData<Integer> getPosition()
-//    {
-//        return mpos;
-//    }
-//
-//    public void setPosition(int pos)
-//    {
-//        mpos.setValue(new Integer(pos));
-//    }
 
     // Create a LiveData with a String
     private MutableLiveData<Integer> mCurrentPosition;
-
-//    public NameViewModel (){
-//        this.mCurrentPosition = new MutableLiveData<>();
-//    }
 
     public MutableLiveData<Integer> getmCurrentPosition() {
         if (mCurrentPosition == null) {
@@ -61,14 +16,6 @@ public class NameViewModel extends ViewModel {
         }
         return mCurrentPosition;
     }
-
-//    public LiveData<Integer> getmCurrentPosition(){
-//        return mCurrentPosition;
-//    }
-
-//    public void setmCurrentPosition(int pos){
-//        mCurrentPosition.setValue(new Integer(pos));
-//    }
 
 
 }
--- app/src/main/java/fr/essant/basilebyvanu/clv/Objet/MenuChapitre.java
+++ app/src/main/java/fr/essant/basilebyvanu/clv/Objet/MenuChapitre.java
@@ -1,22 +1,14 @@
 package fr.essant.basilebyvanu.clv.Objet;
 
 import android.content.Context;
-import android.telephony.AccessNetworkConstants;
 import android.util.Log;
 
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MenuChapitre {
@@ -76,12 +68,7 @@ public class MenuChapitre {
 
             this.addChapitres(jsonObject.get("temps").getAsInt(), chp);
         }
-/*
-        for (int i = 0; i < jsonChapitre.size(); i ++){
-            jsonArray = (JsonArray) parser.parse(jsonChapitre.get(i));
-            Chapitre chp = new Chapitre(jsonArray.get(0).toString(), jsonArray.get(1).getAsInt(),jsonArray.get(2).getAsInt());
-            this.addChapitres(jsonArray.get(1).getAsInt(), chp);
-        } */
+
     }
 
     public HashMap<Integer, Chapitre> getListChapitres() {
