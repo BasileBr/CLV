618877574029e8cdea942913ba2fde93479fb70a
yvan ulas <yulas@enssat.fr>
2019-01-10 10:45:30 +0100

    add sub

--- app/src/test/java/fr/essant/basilebyvanu/clv/ExampleUnitTest.java
+++ app/src/test/java/fr/essant/basilebyvanu/clv/ExampleUnitTest.java
@@ -44,7 +44,7 @@ public class ExampleUnitTest {
 
     @Test
     public void sub_isCorrect(){
-        assertEquals(-8,caisse.sub(-8));
+        assertEquals(-8,caisse.sub(8));
         assertEquals(8,caisse.sub(-16));
         assertEquals(6,caisse.sub(-2));
         assertEquals(2,caisse.sub(-4));
