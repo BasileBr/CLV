06d3593440140133fa872c1ef3602f769e8e586b
Basile Bruhat <basile.bruhat@etudiant.univ-rennes1.fr>
2019-01-10 10:42:13 +0100

    Update : caisse

--- app/src/main/java/fr/essant/basilebyvanu/clv/CaisseEnregistreuse.java
+++ app/src/main/java/fr/essant/basilebyvanu/clv/CaisseEnregistreuse.java
@@ -2,9 +2,31 @@ package fr.essant.basilebyvanu.clv;
 
 public class CaisseEnregistreuse {
 
+    private int total;
+
+
     public CaisseEnregistreuse() {
+        total = 0;
+    }
 
+    public void empty(){
+        total = 0;
+    }
 
-
+    public int add(int nombre){
+        total = total + nombre;
+        return total;
+    }
+    public int sub(int nombre){
+        total = total - nombre;
+        return total;
+    }
+    public int mult(int nombre){
+        total = total * nombre;
+        return total;
+    }
+    public int div(int nombre){
+        total = total / nombre;
+        return total;
     }
 }
